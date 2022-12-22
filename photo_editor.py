1e67db65cf294dbfaa37fb0683cf42d540cf6075
Ana Oliveira <anaf.oliveira95@gmail.com>
2022-12-22 18:54:55 +0000

    photo enhancer

--- photo_editor.py
+++ photo_editor.py
@@ -1,1 +1,27 @@
-o
+import os
+from PIL import Image, ImageFilter
+
+
+def edit_photo(path):
+    path_out = "./edited/"
+
+    if not os.path.exists(path):
+        return -1
+
+    if not os.path.exists(path_out):
+        os.makedirs(path_out)
+
+    for photo_dir in os.listdir(path):
+        img = Image.open(f"{path}/{photo_dir}")
+        edit = img.filter(ImageFilter.DETAIL)
+
+        name_pic = os.path.splitext(photo_dir)[0]
+        full_dir_out = os.path.join(path_out, f"{name_pic}_edited.jpg")
+        print("dir: ",full_dir_out)
+        edit.save(full_dir_out)
+        print("finish")
+
+
+if __name__ == '__main__':
+    in_dir = "./photos/"
+    edit_photo(in_dir)
