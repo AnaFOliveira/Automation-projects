84e2e16996ba06cc886b871a302308d2d993942d
Ana Oliveira <anaf.oliveira95@gmail.com>
2022-12-22 20:15:26 +0000

    Inserting Video downloader Ignoring download

---
+++ youtube_downloader.py
@@ -0,0 +1,27 @@
+from pytube import YouTube
+from sys import argv
+import os
+
+
+def downloader(link_to_download):
+
+    video = YouTube(link_to_download)
+    path_out = "./videos/"
+
+    print("Title: ", video.title)
+    print("Views: ", video.views)
+    print("Author: ", video.author)
+    print("Date ", video.publish_date)
+
+    if not os.path.exists(path_out):
+        print("Folder doesn't exist. Creating folder")
+        os.makedirs(path_out)
+
+    res_download = video.streams.get_lowest_resolution()
+    res_download.download(os.path.join(path_out, video.title))
+    return 1
+
+
+if __name__ == '__main__':
+    link = argv[1]
+    downloader(link)
