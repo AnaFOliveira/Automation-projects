b5c7f86913c457e880bd4e00cc503c5048e3fae0
Ana Oliveira <anaf.oliveira95@gmail.com>
2022-12-22 22:50:22 +0000

    Inserting Pdf merger Ignoring pdfs

---
+++ pdf_merger.py
@@ -0,0 +1,18 @@
+from PyPDF2 import PdfFileMerger
+import os
+
+
+def pdf_merger(pdf_dir):
+    merger = PdfFileMerger()
+    for file in os.listdir(pdf_dir):
+        pdf = os.path.join(pdf_dir, file)
+        if file.endswith(".pdf"):
+            merger.append(pdf)
+    merger.write("pdfs/combined_pdfs.pdf")
+    merger.close()
+    print("PDF merged")
+
+
+if __name__ == '__main__':
+    pdf_folder = "./pdfs/"
+    pdf_merger(pdf_folder)
